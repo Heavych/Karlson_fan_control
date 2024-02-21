f4de17eb03e758debac065303944fe0094422a94
Igor Mukaev <mukaevim@gmail.com>
2024-02-21 11:14:24 +0300

    Update karlson_fan_control.ino
    Этот код управляет вентилятором с использованием кнопок увеличения/уменьшения скорости, мигает светодиодом при изменении скорости, выводит данные в Serial Monitor и включает светодиод предупреждения, если обороты вентилятора ниже порогового значения (2200 об/мин в данном случае).

--- Arduino/karlson_fan_control.ino
+++ Arduino/karlson_fan_control.ino
@@ -1,83 +1,86 @@
-
-int fanPwm = 3;             // Fan PWM pin 
-int fanTach   = 2;          // Fan Tach pin 
-int buttonUp = 7;           // Button speed up pin 
-int buttonDown = 8;         // Button speed down pin 
-int buttonStateUp = 0;      // Button speed up state
-int buttonStateDown = 0;    // Button speed down state
-int pwm = 0;                // 0-250 Pwm duty
-int pwmStep = 25;           // 0-250 Step = 10% (25)
-int led = 13;               // LED pin
-
+int fanPwm = 3;             // Пин PWM для управления вентилятором
+int fanTach = 2;             // Пин для чтения сигнала оборотов вентилятора
+int buttonUp = 7;           // Пин кнопки увеличения скорости вентилятора
+int buttonDown = 8;         // Пин кнопки уменьшения скорости вентилятора
+int buttonStateUp = 0;      // Состояние кнопки увеличения скорости
+int buttonStateDown = 0;    // Состояние кнопки уменьшения скорости
+int pwm = 0;                // Значение ШИМ (Широтно-Импульсная Модуляция) для управления скоростью вентилятора
+int pwmStep = 25;           // Шаг изменения ШИМ (10%)
+int led = 13;               // Пин светодиода
 unsigned long pulseDuration;
 unsigned long previousMillis = 0;
 
-const long interval = 1000; // 1 sec
+const long interval = 1000;  // Интервал для вывода данных в миллисекундах (1 секунда)
 
 void setup() {
   Serial.begin(9600);
-  pinMode(fanPwm, OUTPUT);
-  pinMode(fanTach, INPUT);
-  digitalWrite(fanTach,HIGH);
-  pinMode(led, OUTPUT);
-  pinMode(buttonUp,INPUT_PULLUP);
-  pinMode(buttonDown,INPUT_PULLUP);
+  pinMode(fanPwm, OUTPUT);  // Установка пина управления вентилятором как выход
+  pinMode(fanTach, INPUT);  // Установка пина для чтения сигнала оборотов вентилятора как вход
+  digitalWrite(fanTach, HIGH);  // Включение внутреннего подтягивающего резистора для сигнала оборотов
+  pinMode(led, OUTPUT);  // Установка пина светодиода как выход
+  pinMode(buttonUp, INPUT_PULLUP);  // Установка пина кнопки увеличения скорости как вход с подтягивающим резистором
+  pinMode(buttonDown, INPUT_PULLUP);  // Установка пина кнопки уменьшения скорости как вход с подтягивающим резистором
 }
 
 void loop() {
   buttonStateUp = digitalRead(buttonUp);
   buttonStateDown = digitalRead(buttonDown);
-  
-  pulseDuration = pulseIn(fanTach, LOW);
-  double frequency = 1000000/pulseDuration;
-  int rpm = frequency/2*60;                 
-  
+
+  pulseDuration = pulseIn(fanTach, LOW);  // Измерение длительности импульса сигнала оборотов вентилятора
+  double frequency = 1000000.0 / pulseDuration;  // Расчет частоты вентилятора
+  int rpm = frequency / 2 * 60;  // Расчет оборотов в минуту
+
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    Serial.print("pulse duration: ");
+    Serial.print("Длительность импульса: ");
     Serial.println(pulseDuration);
-    Serial.print("PWM Duty: ");
+    Serial.print("ШИМ: ");
     Serial.println(pwm);
-    Serial.print("RPM: ");
+    Serial.print("Обороты вентилятора: ");
     Serial.println(rpm);
     Serial.println(" ");
   }
+
+  // Увеличение скорости вентилятора при нажатии кнопки увеличения
   if (buttonStateUp == 0 && pwm < 250) {
-    fastBlinkLed();
-     pwm = pwm + pwmStep;
-    analogWrite(fanPwm, pwm);
+    fastBlinkLed();  // Мигание светодиода
+    pwm = pwm + pwmStep;  // Увеличение ШИМ
+    analogWrite(fanPwm, pwm);  // Управление скоростью вентилятора
     Serial.println(pwm);
     if (pwm >= 255) {
       pwm = 255;
     }
     delay(960);
   }
+
+  // Уменьшение скорости вентилятора при нажатии кнопки уменьшения
   if (buttonStateDown == 0 && pwm != 0) {
-    fastBlinkLed();
-    pwm = pwm - pwmStep;
-    analogWrite(fanPwm, pwm);
+    fastBlinkLed();  // Мигание светодиода
+    pwm = pwm - pwmStep;  // Уменьшение ШИМ
+    analogWrite(fanPwm, pwm);  // Управление скоростью вентилятора
     Serial.println(pwm);
-        if (pwm <= 0) {
+    if (pwm <= 0) {
       pwm = 0;
     }
     delay(960);
-  }
-  else {
+  } else {
     analogWrite(fanPwm, pwm);
   }
-  warningLed(rpm);
+
+  warningLed(rpm);  // Включение светодиода предупреждения в зависимости от оборотов вентилятора
 }
 
+// Включение светодиода предупреждения при низких оборотах
 void warningLed(int rpm) {
   if (rpm <= 2200) {
     digitalWrite(led, HIGH);
-  }
-  else {
+  } else {
     digitalWrite(led, LOW);
   }
 }
 
+// Быстрое мигание светодиода
 void fastBlinkLed() {
   digitalWrite(led, HIGH);
   delay(40);
